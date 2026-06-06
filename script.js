3a6660bb408f79301f353438491fdb09a22ecc2d
dzakyalmerkhafid-bit <dzakyalmerkhafid@gmail.com>
2026-06-06 08:50:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("orderBtn").addEventListener("click", function(){
+
+    alert(
+        "✓ ORDER BERHASIL\n\n" +
+        "Terima kasih telah berbelanja di ROG Store.\n" +
+        "Pesanan Anda akan segera kami proses."
+    );
+
+});
